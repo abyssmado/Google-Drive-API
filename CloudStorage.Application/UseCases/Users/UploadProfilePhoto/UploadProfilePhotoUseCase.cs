59ff912d7b2fc05f6bb79d398f10ddc1c6867adb
Denis Amorim <85955679+abyssmado@users.noreply.github.com>
2024-03-13 11:33:23 -0300

    Update UploadProfilePhotoUseCase.cs

--- CloudStorage.Application/UseCases/Users/UploadProfilePhoto/UploadProfilePhotoUseCase.cs
+++ CloudStorage.Application/UseCases/Users/UploadProfilePhoto/UploadProfilePhotoUseCase.cs
@@ -38,9 +38,9 @@ public class UploadProfilePhotoUseCase : IUploadProfilePhotoUseCase
             Name = "Denis Amorim",
             Email = "denisamorim.teste@gmai.com",
             RefreshToken =
-                "1//04S6CnwzcrJYbCgYIARAAGAQSNgF-L9Ir6hTCRCjU45xg9Nt7qx51EvgzrxgegwiMRMmiqPFsb30q2tJPuXPHn80F1N766U5WoQ",
+                "your_refresh_token",
             AccessToken =
-                "ya29.a0Ad52N3-fUrpZUcK7QFUII3RyQcqoeg_2X07DXmC-B92z09LqbxTSnze3oJLJ5QHpilU5BLU5dTlPVjHXucFnvI021tfFbmMYN0_6QsKU6YGGUPEFsVwz803UHgEY3k0U_BqYOknSiH-TbyTY8JO_1UNDY0W2m5NnqMdsaCgYKAZ8SARASFQHGX2MivslhbYnnVifkQ4BN6dlKnQ0171"
+                "your_access_token"
         };
     }
 }
